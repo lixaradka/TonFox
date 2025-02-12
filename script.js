e5f464ead4f5c1c5205049f8dd303996e4078378
lixaradka <kozlovnikon907@gmail.com>
2025-02-12 21:52:16 +0100

    commit3

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-// Инициализация TON Connect
 const tonConnectUI = new TonConnectUI({
-  manifestUrl: "https://lixaradka.github.io/TonFox/tonconnect-manifest.json",
-});
-
-// Кнопка "Connect Wallet"
-document.getElementById('connect-wallet').addEventListener('click', async () => {
-  try {
-    await tonConnectUI.connectWallet();
-    alert('Кошелек подключен!');
-  } catch (error) {
-    console.error('Ошибка подключения:', error);
-  }
-});
-
-// Отслеживание изменений статуса кошелька
-tonConnectUI.onStatusChange(wallet => {
-  if (wallet) {
-    const userAddress = wallet.account.address;
-    console.log('Адрес кошелька:', userAddress);
-    // Сохраните адрес в localStorage или отправьте на бэкенд
-  } else {
-    console.log('Кошелек отключен.');
-  }
-});
+    manifestUrl: "https://tonfox-3rcc99toi-lixaradkas-projects.vercel.app/tonconnect-manifest.json",
+  });
+  
+  // Кнопка "Connect Wallet"
+  document.getElementById('connect-wallet').addEventListener('click', async () => {
+    try {
+      await tonConnectUI.connectWallet();
+      alert('Кошелек подключен!');
+    } catch (error) {
+      console.error('Ошибка подключения:', error);
+    }
+  });
+  
+  // Отслеживание изменений статуса кошелька
+  tonConnectUI.onStatusChange(wallet => {
+    if (wallet) {
+      const userAddress = wallet.account.address;
+      console.log('Адрес кошелька:', userAddress);
+      // Сохраните адрес в localStorage или отправьте на бэкенд
+    } else {
+      console.log('Кошелек отключен.');
+    }
+  });
